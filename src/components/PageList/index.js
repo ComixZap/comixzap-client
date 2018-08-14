25192453f8fdcdac129d82074c1de509d76b6e82
Richard Hoffman <coverslide@gmail.com>
2018-08-13 17:59:50 -0700

    Use latest files api, add touch events

--- src/components/PageList/index.js
+++ src/components/PageList/index.js
@@ -18,12 +18,12 @@ export default class PageList extends Component {
       console.log(path, file.filename)
       const response = await fetch(`${this.props.config.root}${encodePath(joinPath(path, file.filename))}?action=contents`);
       const pagesList = await response.json();
-      const pagesFiltered = pagesList.files.filter(page => (page.size && EXTENSION_WHITELIST.includes(extname(page.name.toLowerCase()))));
-      const pages = sortBy(pagesFiltered, p => p.name.toLowerCase());
+      const pagesFiltered = pagesList.files.filter(page => (page.usize && EXTENSION_WHITELIST.includes(extname(page.filename.toLowerCase()))));
+      const pages = sortBy(pagesFiltered, p => p.filename.toLowerCase());
       this.setState({ loading: false, pages, currentPage: 1 });
       this.props.onLoad(pages);
       if (pages.length) {
-        this.props.onPageClick(path, file, pages[0].name);
+        this.props.onPageClick(path, file, pages[0].filename);
       }
     } catch (err) {
       this.setState({ loading: false, error: err.message });
@@ -46,7 +46,7 @@ export default class PageList extends Component {
     if (nextPage > pages.length) {
       return;
     }
-    const page = pages[nextPage - 1].name;
+    const page = pages[nextPage - 1].filename;
     this.setState({ currentPage: nextPage });
     this.props.onPageClick(path, file, page);
   }
@@ -57,7 +57,7 @@ export default class PageList extends Component {
     if (nextPage < 1) {
       return;
     }
-    const page = pages[nextPage - 1].name;
+    const page = pages[nextPage - 1].filename;
     this.setState({ currentPage: nextPage });
     this.props.onPageClick(path, file, page);
   }
@@ -66,14 +66,14 @@ export default class PageList extends Component {
     const { file, error, loading, pages, currentPage } = this.state;
     return (
       <div ref={r => this.root = r} className="page-list">
-        {file && <h5>{file.name}</h5> }
+        {file && <h5>{file.filename}</h5> }
         {error && <div style={{ color: 'red' }}>Error: {error}</div>}
         {loading && <h6>Loading ...</h6>}
         {!pages.length ? null : (
           <ol>
             {pages.map((page, index) => (
-              <li className={c("page", { highlighted: (index + 1) === currentPage })} onClick={this.curryOnPageClick(index, page.name)} key={page.name}>
-                {page.name}
+              <li className={c("page", { highlighted: (index + 1) === currentPage })} onClick={this.curryOnPageClick(index, page.filename)} key={page.filename}>
+                {page.filename}
               </li>
             ))}
           </ol>
--- src/components/Viewer/index.js
+++ src/components/Viewer/index.js
@@ -3,8 +3,13 @@ import { join as joinPath } from 'path';
 import c from 'classnames';
 import { encodePath } from '../../utils';
 
+const supportsTouch = 'ontouchstart' in window;
+
+const calcDistance = (x1, y1, x2, y2) => Math.pow(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2), .5);
+
 export default class Viewer extends Component {
   state = { zoom: 100 };
+  initialDistance = 0;
 
   async load (path, file, page) {
     this.setState({ loading: true, page });
@@ -23,9 +28,33 @@ export default class Viewer extends Component {
     }
   }
 
-  onDoubleClick = () => {
-    this.setState({ zoom: ((this.state.zoom === 100) ? 200 : 100) });
+  onTouchEnd = (e) => {
+
   }
+
+  onTouchMove = (e) => {
+    if (e.touches.length >= 2) {
+      const touch1 = e.touches[0];
+      const touch2 = e.touches[1];
+      const distance = calcDistance(touch1.screenX, touch1.screenY, touch2.screenX, touch2.screenY);
+      const sizeFactor = distance / this.initialDistance;
+      this.setState({ zoom: this.initialZoom * sizeFactor });
+    }
+  }
+
+  onTouchStart = (e) => {
+    if (e.touches.length == 2) {
+      const touch1 = e.touches[0];
+      const touch2 = e.touches[1];
+      this.initialDistance = calcDistance(touch1.screenX, touch1.screenY, touch2.screenX, touch2.screenY);
+      this.initialZoom = this.state.zoom;
+    }
+  }
+
+  onDoubleClick = () => {
+    this.setState({ zoom: this.state.zoom > 100 ? 100 : 200 });
+  }
+
 
   render () {
     const { image, loading, error, zoom } = this.state;
@@ -33,7 +62,7 @@ export default class Viewer extends Component {
     return (
       <div ref={r => this.root = r} className={c("image-viewer", { loading })}>
         {error && <div className="error-overlay">{error}</div>}
-        <div className="image" onDoubleClick={this.onDoubleClick}>
+        <div className="image" onDoubleClick={this.onDoubleClick} onTouchEnd={this.onTouchEnd} onTouchMove={this.onTouchMove} onTouchStart={this.onTouchStart}>
           {image && <img title={this.props.page} alt={this.props.page} src={image} style={{ width: `${zoom}%`}} />}
         </div>
       </div>
