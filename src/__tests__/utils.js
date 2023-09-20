c4c0544789addd8abde053fb289694641856487c
Richard Hoffman <coverslide@gmail.com>
2023-09-20 10:42:43 -0500

    Cleanup, fixes

--- src/__tests__/utils.js
+++
@@ -1,7 +0,0 @@
-import { encodePath } from '../utils';
-
-describe('encodePath', () => {
-  it('should encode a path with special characters', () => {
-    expect(encodePath('a ? & # b/c d.e')).toBe('a%20%3F%20%26%20%23%20b/c%20d.e');
-  });
-});
